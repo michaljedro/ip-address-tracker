d2578ff06c8233b6cc00cc290c65151c90e39ad5
Michał Jędro <paw3l.schmidt@gmail.com>
2022-04-30 11:35:52 +0200

    tidying codebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import Input from './components/Input/Input'
-import GeoInfo from './components/GeoInfo/GeoInfo'
 import Header from './components/Header/Header'
 
 function App() {
   return (
     <div>
-      {/*<Input placeholder="Search for any IP address or domain" />*/}
-      {/*<GeoInfo ipAddress="192.158.144.194" location="Brooklyn, NY 10001" timezone="UTC -05:00" isp="SpaceX Starlink" />*/}
       <Header />
     </div>
   );
